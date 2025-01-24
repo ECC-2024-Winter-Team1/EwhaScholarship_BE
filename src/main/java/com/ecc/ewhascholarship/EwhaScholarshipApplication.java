b74ef964f4c8bddfa4252ceaa742ac8affcacf50
haeun228 <123haeun@naver.com>
2025-01-24 10:09:15 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/ecc/ewhascholarship/EwhaScholarshipApplication.java
@@ -0,0 +1,13 @@
+package com.ecc.ewhascholarship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwhaScholarshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwhaScholarshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecc/ewhascholarship/EwhaScholarshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecc.ewhascholarship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhaScholarshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
