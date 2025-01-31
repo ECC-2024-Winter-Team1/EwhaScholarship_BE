403df5f6de0d7a300b678a8725f1b572194c6e20
haeun228 <123haeun@naver.com>
2025-01-31 10:46:08 +0900

    Feat: SecurityConfig 초기 설정

---
+++ src/main/java/com/ecc/ewhascholarship/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ecc.ewhascholarship.config;
+
+import com.ecc.ewhascholarship.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
+        return http.build();
+    }
+
+}
