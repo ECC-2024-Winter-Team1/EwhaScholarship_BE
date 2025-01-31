1bb349fa7bee31a4fe7474cf7ad8a5232fc09eff
haeun228 <123haeun@naver.com>
2025-01-31 13:11:07 +0900

    Feat: API 공통 response 구현

---
+++ src/main/java/com/ecc/ewhascholarship/common/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.ecc.ewhascholarship.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status; // "success" 또는 "error"
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>("success", message, data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>("error", message, null);
+    }
+
+}
