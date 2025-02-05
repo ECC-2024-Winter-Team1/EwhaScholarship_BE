1dc24b98540e79907b6c9ffc55a92d02c9bd0304
haeun228 <123haeun@naver.com>
2025-02-06 02:33:08 +0900

    Feat: JWT 인증 필터 구현

---
+++ src/main/java/com/ecc/ewhascholarship/security/OAuth2UserPrincipal.java
@@ -0,0 +1,30 @@
+package com.ecc.ewhascholarship.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@AllArgsConstructor
+public class OAuth2UserPrincipal implements OAuth2User {
+    private String userId;
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return userId;
+    }
+}
