cd9375e80c8ff0b4b7481435b50164f3c5178bd1
haeun228 <123haeun@naver.com>
2025-02-11 01:36:05 +0900

    Feat: 특정 장학금 조회 API 구현

--- src/main/java/com/ecc/ewhascholarship/controller/ScholarshipController.java
+++ src/main/java/com/ecc/ewhascholarship/controller/ScholarshipController.java
@@ -1,7 +1,30 @@
 package com.ecc.ewhascholarship.controller;
 
+import com.ecc.ewhascholarship.common.ApiResponse;
+import com.ecc.ewhascholarship.dto.ScholarshipDetailDto;
+import com.ecc.ewhascholarship.service.ScholarshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@RequestMapping("/api/scholarships")
 public class ScholarshipController {
+
+    @Autowired
+    private ScholarshipService scholarshipService;
+
+    @GetMapping("/{scholarshipId}")
+    public ResponseEntity<ApiResponse<ScholarshipDetailDto>> getScholarship(@PathVariable Long scholarshipId) {
+        ScholarshipDetailDto response = scholarshipService.getScholarshipById(scholarshipId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success("특정 장학금 조회 성공!", response));
+    }
 }
--- src/main/java/com/ecc/ewhascholarship/service/ScholarshipService.java
+++ src/main/java/com/ecc/ewhascholarship/service/ScholarshipService.java
@@ -1,7 +1,12 @@
 package com.ecc.ewhascholarship.service;
 
+import com.ecc.ewhascholarship.common.ApiResponse;
+import com.ecc.ewhascholarship.domain.Scholarship;
+import com.ecc.ewhascholarship.dto.ScholarshipDetailDto;
 import com.ecc.ewhascholarship.repository.ScholarshipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +14,17 @@ public class ScholarshipService {
 
     @Autowired
     private ScholarshipRepository scholarshipRepository;
+
+    // 특정 장학금 조회
+    public ScholarshipDetailDto getScholarshipById(Long scholarshipId) {
+        Scholarship scholarship = scholarshipRepository.findById(scholarshipId).orElse(null);
+        System.out.println(scholarship.toString());
+
+        if (scholarship==null) {
+            throw new IllegalArgumentException("해당 장학금을 찾을 수 없습니다.");
+        }
+
+        return ScholarshipDetailDto.fromEntity(scholarship);
+    }
 
 }
