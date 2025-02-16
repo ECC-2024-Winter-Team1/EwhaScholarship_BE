7a689c20ad66b948d304ea43d14dd08498aaed7d
haeun228 <123haeun@naver.com>
2025-02-17 01:42:22 +0900

    Refactor: Criteria 테이블 삭제 & 개별 조건 테이블 생성

--- src/main/java/com/ecc/ewhascholarship/domain/ScholarshipIncomeLevel.java
+++ src/main/java/com/ecc/ewhascholarship/domain/ScholarshipIncomeLevel.java
@@ -1,28 +1,20 @@
 package com.ecc.ewhascholarship.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 @Entity
-public class Criteria {
+public class ScholarshipIncomeLevel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne
     @JoinColumn(name = "scholarship_id", nullable = false)
     private Scholarship scholarship;
 
     @Column(nullable = false)
-    private String criteriaType;
-
-    @Column(nullable = false)
-    private String criteriaValue;
+    private Integer incomeLevel;
 
 }
+
