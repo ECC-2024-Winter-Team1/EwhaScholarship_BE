c3db01553d52d5f81f16f167372daac6a04505c8
haeun228 <123haeun@naver.com>
2025-02-18 11:18:19 +0900

    Refactor: ApiResponse 응답 구조 개선

--- src/main/java/com/ecc/ewhascholarship/common/ApiResponse.java
+++ src/main/java/com/ecc/ewhascholarship/common/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.ecc.ewhascholarship.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class ApiResponse<T> {
     private String status; // "success" 또는 "error"
     private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     public static <T> ApiResponse<T> success(String message, T data) {
